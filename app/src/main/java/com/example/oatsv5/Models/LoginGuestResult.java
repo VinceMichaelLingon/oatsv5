f026a9862e5f7a8b7f80eb4ee7ddd3668efbcf3b
vincemichaellingon <vincemichaellingon@gmail.com>
2022-04-10 02:58:25 +0800

    update recycler view withonclick

---
+++ app/src/main/java/com/example/oatsv5/Models/LoginGuestResult.java
@@ -0,0 +1,18 @@
+package com.example.oatsv5.Models;
+
+import com.google.gson.annotations.SerializedName;
+
+public class LoginGuestResult {
+    private String email;
+
+
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
